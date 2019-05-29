6841e3438da07f40757f416f2f736f657dc1c6c3
Michal Prokeš <prokmi@gmail.com>
2019-05-29 22:24:41 +0200

    changed discord key

--- __auth/_auth.py
+++ __auth/_auth.py
@@ -1,5 +1,5 @@
 class Auth:
 
-    DISCORD_KEY = 'NTgzMzI0OTE4MTM4MjczNzky.XO6twA.5CfFoDYwDEpewhUOvx_xxkn9Yj4'
+    DISCORD_KEY = 'NTgzMzI0OTE4MTM4MjczNzky.XO7oig.au696KaZZpX0O4oBhCjDYItZ040'
     BNET_KEY = '2d02382ce3ca4dc9b1f5ec28fe5b969d'
     BNET_SECRET_KEY = 'ttAgLCXT7WBF9EgePvFhvKtwReMseDSj'
