cedcb9ba35bcd38540238fd132e1325c5aea2ba3
Michal Prokeš <prokmi@gmail.com>
2019-05-30 20:52:39 +0200

    Only names formated?

--- armory.py
+++ armory.py
@@ -243,8 +243,8 @@ class ArmoryAPI:
             elif not "gem v" in current_member:
                 gemmed = ":white_check_mark:"
 
-            spaces = (max_len - len(guild_member['name'])) * "."
-            message = f"|{guild_member['name']}{spaces}|    {enchanted}    |    {gemmed}    |\n"
+            spaces = (max_len - len(guild_member['name'])) * " "
+            message = f"|```{guild_member['name']}```{spaces}|    {enchanted}    |    {gemmed}    |\n"
             result = result + message
 
         return result
