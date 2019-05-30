0a7129e5a9b03feaed517c43bb4c2bc406def22c
Michal Prokeš <prokmi@gmail.com>
2019-05-30 21:47:02 +0200

    spaces in formatting

--- armory.py
+++ armory.py
@@ -249,7 +249,7 @@ class ArmoryAPI:
                 gemmed = ":white_check_mark:"
 
             spaces = (max_len - len(guild_member['name'])) * " "
-            message = f"|`{guild_member['name']}`{spaces}|    {enchanted}    |    {gemmed}    |\n"
+            message = f"|`{guild_member['name']}{spaces}`|    {enchanted}    |    {gemmed}    |\n"
             result = result + message
 
         return result
